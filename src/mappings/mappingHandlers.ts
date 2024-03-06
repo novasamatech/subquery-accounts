a6bdb444b2f2151fb3b6ee2974e1bed9a81fb724
asmadek <alma2610@ya.ru>
2024-03-06 12:04:59 +0500

    fix: remove unused fields

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -6,15 +6,12 @@ import { PureProxy } from "../types";
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
   const {
     event: {
-      data: [accountId, proxyAccountId, proxyType, delay],
+      data: [accountId],
     },
   } = event;
 
   const pureProxy = PureProxy.create({
     id: accountId.toHex(),
-    proxyAccountId: proxyAccountId.toHex(),
-    proxyType: proxyType.toString(),
-    delay: Number(delay.toString()),
   });
 
   await pureProxy.save();
