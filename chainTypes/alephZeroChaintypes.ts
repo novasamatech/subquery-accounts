9f924d47517ee17badd4e7ef78f9c6e59fce228f
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2024-10-16 12:43:10 +0300

    feat: adding new networks for #2392 (#16)

---
+++ chainTypes/alephZeroChaintypes.ts
@@ -0,0 +1,22 @@
+// Copyright 2017-2023 @polkadot/types-known authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable sort-keys */
+
+import type { OverrideBundleDefinition } from "@polkadot/types/types";
+
+// structs need to be in order
+/* eslint-disable sort-keys */
+
+const definitions: OverrideBundleDefinition = {
+  types: [
+    {
+      minmax: [3, undefined],
+      types: {
+        DispatchErrorModule: "DispatchErrorModuleU8",
+      },
+    },
+  ],
+};
+
+export default { typesBundle: { spec: { "aleph-node": definitions } } };
