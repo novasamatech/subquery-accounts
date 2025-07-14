5edd642d94a7980d6181e3080f71de0c66c8e6b3
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-07-14 22:42:01 +0300

    fix: if can't transfer to bytes

--- src/utils/isJson.ts
+++ src/utils/isJson.ts
@@ -3,5 +3,5 @@ import { Extrinsic } from "@polkadot/types/interfaces";
 
 export const isJsonStringArgs = (extrinsic: Extrinsic) => {
   //123 is {  and  125 is }        :)
-  return (extrinsic.args[0] as Bytes).at(-1) === 125 && (extrinsic.args[0] as Bytes).at(0) === 123;
+  return (extrinsic.args[0] as Bytes)?.at(-1) === 125 && (extrinsic.args[0] as Bytes)?.at(0) === 123;
 }
