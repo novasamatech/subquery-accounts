b6c90e1c6e712e06ce613858a406036d6cabe0be
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-07-14 22:53:33 +0300

    fix: check for at is a function

--- src/utils/isJson.ts
+++ src/utils/isJson.ts
@@ -2,6 +2,11 @@ import { Bytes } from "@polkadot/types";
 import { Extrinsic } from "@polkadot/types/interfaces";
 
 export const isJsonStringArgs = (extrinsic: Extrinsic) => {
+  const bytesArg = extrinsic.args[0] as Bytes;
+  if (!bytesArg || !(typeof bytesArg.at === 'function')) {
+    return false;
+  }
+
   //123 is {  and  125 is }        :)
-  return (extrinsic.args[0] as Bytes)?.at(-1) === 125 && (extrinsic.args[0] as Bytes)?.at(0) === 123;
+  return bytesArg.at(-1) === 125 && bytesArg.at(0) === 123;
 }
