f56fbf31c515e225da920fd6be1c783409cbd293
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2025-07-24 14:01:51 +0200

    chore: multichain main merge (#51)
    * fix: rejected operation whon't delete call data (#35)
    
    * fix: rejected operation whon't delete call data
    
    * fix: type assignment
    
    * fix: chain_id in model (#41)
    
    * fix: speed up indexing (#42)
    
    * fix: speed up indexing
    
    * Update westend-asset-hub.yaml
    
    * Update westend-asset-hub.yaml
    
    * fix: remark parse muy rapido (#44)
    
    * fix: remark parse muy rapido
    
    * fix: lib upgreade
    
    * fix: order
    
    * feat: utility batch call walker (#47)
    
    * feat: utility batch call walker
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: style
    
    * fix: redo remark handling using call handlers, prettier (#49)
    
    ---------
    
    Co-authored-by: Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
    Co-authored-by: Vitya Livshits <64482828+cuteWarmFrog@users.noreply.github.com>

--- src/utils/checkAndGetAccount.ts
+++ src/utils/checkAndGetAccount.ts
@@ -1,12 +1,6 @@
-import { decodeAddress, encodeAddress } from "../utils";
 import { Account } from "../types";
-import { u8aToHex } from "@polkadot/util";
 
-export async function checkAndGetAccount(
-  pubKey: string,
-  isMultisig = false,
-  threshold = 0
-): Promise<Account> {
+export async function checkAndGetAccount(pubKey: string, isMultisig = false, threshold = 0): Promise<Account> {
   let account = await Account.get(pubKey);
   if (!account) {
     // We couldn't find the account
