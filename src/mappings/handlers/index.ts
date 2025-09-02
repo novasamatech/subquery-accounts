783a6e126aa8aca841c5f6b0911aa6946f00d889
cuteWarmFrog <livshits.victor@gmail.com>
2025-09-02 17:57:42 +0200

    fixed some shit

--- src/mappings/handlers/index.ts
+++ src/mappings/handlers/index.ts
@@ -1,14 +1,10 @@
 export { handleProxyProxy, handleMultisig, handleBatch } from "./generic";
 
-export { handleMultisigCall, handleMultisigInProxy } from "./multisigCallHandler";
+export { handleMultisigCall } from "./multisigCallHandler";
 export { handlePureProxyEvent, handlePureProxyKilledEvent } from "./pureProxyEventHandler";
 export { handleProxyEvent, handleProxyRemovedEvent } from "./proxyEventHandler";
 export { handleProxyCall } from "./proxyCallHandler";
 export { handleAssetHubMigrationEvent } from "./assetHubMigrationHandler";
 export { handleRemark } from "./multisigRemarkHandler";
-export { handleProxyProxy, handleMultisig, handleBatch } from "./generic";
 
-export { handleMultisigCall } from "./multisigCallHandler";
 export { handleNewMultisigEvent, handleMultisigApprovedEvent, handleMultisigExecutedEvent, handleMultisigCancelledEvent } from "./multisigEventHandler";
-export { handlePureProxyEvent } from "./pureProxyEventHandler";
-export { handleRemark } from "./multisigRemarkHandler";
--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,6 +1,4 @@
 export {
-
-  handleMultisigInProxy,
   handleProxyEvent,
   handleProxyCall,
   handleProxyRemovedEvent,
